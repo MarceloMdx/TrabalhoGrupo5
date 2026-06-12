7899c95717ca8bce8c756dfe42bbf7c1bdbac406
VReis777 <h.reis777@gmail.com>
2026-06-11 21:41:07 -0300

    Corrigindo Erros bobos

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
+
 import './global.css'
 import "./variables.css";
 import App from './App.jsx'
@@ -9,6 +10,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
